47ec7f10380fc961f3d0e5909be45d6674f7427e
Heaklinh <heaklinh@gmail.com>
2024-03-22 17:29:54 +0700

    Modify Path

--- features/product/widgets/drink_tile.py
+++ features/product/widgets/drink_tile.py
@@ -1,18 +1,16 @@
+
 from pathlib import Path
 import tkinter as tk
 from tkinter import Button, Label, PhotoImage
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
-
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH.parent.parent.parent / "assets/frame0"
 # ASSETS_PATH = OUTPUT_PATH / Path(r"Monitor_App/assets/frame1")
 
-
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
-
 
 class DrinkTile(tk.Frame):
     def __init__(self, parent, drink_id,drink_flavour, drink_price, drink_description, drink_image):
@@ -22,13 +20,11 @@ class DrinkTile(tk.Frame):
         self.drink_price = drink_price
         self.drink_description = drink_description
         self.drink_image = drink_image
-
         self.create_widgets()
         self.pack()
         
         # Bind left mouse button click event to the show_product_detail method
         # self.bind("<Button-1>", self.show_product_detail)
-
     def load_image_from_url(self, url):
         response = requests.get(url)
         img = Image.open(BytesIO(response.content))
@@ -36,7 +32,6 @@ class DrinkTile(tk.Frame):
         
         return ImageTk.PhotoImage(img)
     
-
     def create_widgets(self):
         # button_image_1 = PhotoImage(
         #     file=relative_to_assets("button_1.png"))
@@ -59,11 +54,9 @@ class DrinkTile(tk.Frame):
         image_label.image = image
         image_label.grid(row=0, column=0, rowspan=2, padx=12, pady=12)
         # image_label.bind("<Button-1>", self.show_product_detail)
-
         flavour_label = Label(self, text=self.drink_flavour, font=('Helvetica', 15),bg="white", padx=12)
         flavour_label.grid(row=0, column=1, sticky=tk.W)
         # flavour_label.bind("<Button-1>", self.show_product_detail)
-
         price_label = Label(self, text=f"${self.drink_price:.2f}", font=('Helvetica', 20), bg="white", padx=12)
         price_label.grid(row=1, column=1, sticky=tk.W)
         
